f1cb8d956eb745b6240eff6c9124dd84ce0bea16
Kersten Burkhardt <kerstenk@gmail.com>
2014-09-16 22:34:55 +0200

    loop

--- Service.js
+++ Service.js
@@ -1,10 +1,28 @@
-var gpio = require('rpi-gpio');
+var async = require('async'),
+    gpio = require('rpi-gpio'),
+    isOn = true;
 
-gpio.setup(15, gpio.DIR_OUT, write);
+async.forever(
+    function(next) {
+        gpio.setup(15, gpio.DIR_OUT, write);
 
-function write() {
-    gpio.write(15, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
-}
+        function write () {
+            if (isOn) {
+                isOn = false;
+            } else {
+                isOn = true;
+            }
+
+            gpio.write(15, isOn, function (err) {
+                if (err) throw err;
+
+                setTimeout(next, 3000);
+            });
+        }
+    },
+    function(err) {
+        // if next is called with a value in its first parameter, it will appear
+        // in here as 'err', and execution will stop.
+    }
+);
+
