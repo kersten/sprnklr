6ca41f48585ed39a596b120f00e3b8614266babf
Kersten Burkhardt <kerstenk@gmail.com>
2014-09-16 22:48:48 +0200

    added cpuinfo

--- Service.js
+++ Service.js
@@ -1,28 +1,35 @@
 var async = require('async'),
     gpio = require('rpi-gpio'),
+    proc = require('node-proc'),
     isOn = true;
 
-async.forever(
-    function(next) {
-        gpio.setup(15, gpio.DIR_OUT, write);
+proc.cpuinfo(function (err, cpuinfo) {
+    if (!err) {
+        console.log(cpuinfo);
 
-        function write () {
-            if (isOn) {
-                isOn = false;
-            } else {
-                isOn = true;
+        async.forever(
+            function(next) {
+                gpio.setup(15, gpio.DIR_OUT, write);
+
+                function write () {
+                    if (isOn) {
+                        isOn = false;
+                    } else {
+                        isOn = true;
+                    }
+
+                    gpio.write(15, isOn, function (err) {
+                        if (err) throw err;
+
+                        setTimeout(next, 3000);
+                    });
+                }
+            },
+            function(err) {
+                // if next is called with a value in its first parameter, it will appear
+                // in here as 'err', and execution will stop.
             }
-
-            gpio.write(15, isOn, function (err) {
-                if (err) throw err;
-
-                setTimeout(next, 3000);
-            });
-        }
-    },
-    function(err) {
-        // if next is called with a value in its first parameter, it will appear
-        // in here as 'err', and execution will stop.
+        );
     }
-);
+});
 
