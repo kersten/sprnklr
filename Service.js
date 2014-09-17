792402316fa8fdea75ee46178d7762d963ac77a3
Kersten Burkhardt <kerstenk@gmail.com>
2014-09-17 19:44:54 +0200

    fixed function call

--- Service.js
+++ Service.js
@@ -50,6 +50,8 @@ async.parallel({
                     setTimeout(next, 3000);
                     //});
                 }
+
+                write();
             });
         },
         function(err) {
